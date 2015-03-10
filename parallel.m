4a4d6faa18706e3013274c75f1f31e7418741563
emmaggie <strzelecka.em@gmail.com>
2015-03-10 16:06:20 -0700

    modify ingnored

--- parallel.m
+++
@@ -1,9 +0,0 @@
-if exist('OCTAVE_VERSION') ~= 0  
-    pkg load parallel
-end
-
-inputs = 1:100;  
-numCores = nproc()
-
-% assumes that processInput is defined in a separate function file
-[result] = pararrayfun (numCores, @processInput, inputs)
